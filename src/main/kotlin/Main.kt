289ce7cf82dc32721dd974adf951f2d64ddc6d61
ddung22 <dlgodeh@naver.com>
2024-04-23 17:55:05 +0900

    create

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    val people = listOf(
+        mapOf("name" to "김르탄", "age" to 27),
+        mapOf("name" to "이스파", "age" to 18),
+        mapOf("name" to "최개발", "age" to 54),
+        mapOf("name" to "박코딩", "age" to 32),
+    )
+    println(people)
+
+
+}
