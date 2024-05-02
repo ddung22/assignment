262949f7422a0a668a12130c3c62e82dfd42f6c7
ddung22 <dlgodeh@naver.com>
2024-05-02 12:05:16 +0900

    숫자야구 틀 제작

---
+++ src/main/kotlin/Number base ball.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main(){
+    var count = 0
+
+    println("메뉴를 선택해주세요")
+    println("1. 게임시작")
+    println("2. 기록보기")
+    println("3. 종료")
+    var menu : Int = readLine()!!.toInt()
+
+    println("지금부터 숫자야구 게임을 시작합니다")
+    println("서로다른 숫자 3개를 입력해주세요")
+
+
+}
